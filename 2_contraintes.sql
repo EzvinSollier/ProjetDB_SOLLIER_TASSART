377aaf1e8d0ece558b2c7bc247b3212f495dbf04
Olpr <100537018+EzvinSollier@users.noreply.github.com>
2026-03-11 17:49:04 +0100

    Update constraints

--- 2_contraintes.sql
+++ 2_contraintes.sql
@@ -9,17 +9,16 @@ ALTER TABLE champion ADD CONSTRAINT champion_difficulte_constraint CHECK ( C_Dif
 ALTER TABLE item ADD CONSTRAINT item_prix_constraint CHECK (I_Cout > 0 and I_Cout < 4000);
 
 -- Les contraintes pour les skins vont être au niveau du prix aussi
-ALTER TABLE skin ADD CONSTRAINT skin_prix_constraint CHECK (Sk_Prix > 0 );
+ALTER TABLE skin ADD CONSTRAINT skin_prix_constraint CHECK (Sk_Prix >= 0 );
 
 
 -- Pour les monstres les contraintes vont être au niveau des pv (qu'ils soient positifs et différent de zéro) pour la puissance d'attaque (positifs et différent de zéro) pour l'xp qu'ils donnent (la même chose encore) pour l'argent qu'ils donnent (la même chose) et pour le cycle (la même chose 
-ALTER TABLE monstre ADD CONSTRAINT monstre_hp_constraint CHECK (M_HP > 0);
-ALTER TABLE monstre ADD CONSTRAINT monstre_attaque_constraint CHECK (M_Attaque > 0);
-ALTER TABLE monstre ADD CONSTRAINT monstre_xp_constraint CHECK (M_XP > 0);
-ALTER TABLE monstre ADD CONSTRAINT monstre_argent_constraint CHECK (M_Argent > 0);
-ALTER TABLE monstre ADD CONSTRAINT monstre_cycle_constraint CHECK (M_Cycle > 0);
+ALTER TABLE monstre ADD CONSTRAINT monstre_hp_constraint CHECK (M_HP >= 0);
+ALTER TABLE monstre ADD CONSTRAINT monstre_attaque_constraint CHECK (M_Attaque >= 0);
+ALTER TABLE monstre ADD CONSTRAINT monstre_xp_constraint CHECK (M_XP >= 0);
+ALTER TABLE monstre ADD CONSTRAINT monstre_argent_constraint CHECK (M_Argent >= 0);
+ALTER TABLE monstre ADD CONSTRAINT monstre_cycle_constraint CHECK (M_Cycle >= 0);
 
 
 -- Pour les Sorts les contraintes vont être concernant le temps de recharge du sort ainsi que le ratio (les 2 ne devant être supérieur à 0)
-ALTER TABLE sort ADD CONSTRAINT sort_recharge_constraint CHECK (So_Recharge > 0);
-ALTER TABLE sort ADD CONSTRAINT sort_ratio_constraint CHECK (So_Ratio > 0);
+ALTER TABLE sort ADD CONSTRAINT sort_recharge_constraint CHECK (So_Recharge >= 0);
